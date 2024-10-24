e15c128ed6c86ca53284d1bdf86558fd0e9af6c9
wren <16328864+sharkked@users.noreply.github.com>
2024-10-24 13:30:00 -0500

    fix date issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,51 +1,34 @@
-const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
+const pluginRss = require('@11ty/eleventy-plugin-rss')
 
 module.exports = function (config) {
-  config.addPassthroughCopy("./src/styles");
-  config.addPassthroughCopy({ assets: "/" });
-  config.addPassthroughCopy({ static: "/" });
+  config.addPassthroughCopy('./src/styles')
+  config.addPassthroughCopy({ assets: '/' })
+  config.addPassthroughCopy({ static: '/' })
 
-  config.addWatchTarget("./src/styles/");
-  config.addWatchTarget("./assets/styles/");
+  config.addWatchTarget('./src/styles/')
+  config.addWatchTarget('./assets/styles/')
 
-  config.addFilter("randomItem", (arr) => {
+  config.addFilter('randomItem', (arr) => {
     arr.sort(() => {
-      return 0.5 - Math.random();
-    });
-    return arr.slice(0, 1);
-  });
+      return 0.5 - Math.random()
+    })
+    return arr.slice(0, 1)
+  })
 
-  config.addFilter("utc", (date) => date.toUTCString())
-  config.addFilter("iso", (date) => date.toISOString())
+  config.addFilter('utc', (date) => date.toUTCString())
+  config.addFilter('iso', (date) => date.toISOString())
 
-  config.addFilter("excerpt", (post, len) => {
-    const content = post.replace(/(<([^>]+)>)/gi, "");
-    return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
-  });
+  config.addFilter('excerpt', (post, len) => {
+    const content = post.replace(/(<([^>]+)>)/gi, '')
+    return content.substr(0, content.lastIndexOf(' ', 200)) + '...'
+  })
 
-  config.addPlugin(feedPlugin, {
-    type: "atom",
-    outputPath: "/feed.xml",
-    collection: {
-      name: "post",
-      limit: 15,
-    },
-    metadata: {
-      language: "en",
-      title: "evvil.town",
-      subtitle: "the ramblings of an outdoor shut-in",
-      base: "https://evvil.town/",
-      author: {
-        name: "wren",
-        email: "",
-      }
-    }
-  });
+  config.addPlugin(pluginRss)
 
   return {
     dir: {
-      input: "src",
-      output: "www",
-    },
-  };
-};
+      input: 'src',
+      output: 'www'
+    }
+  }
+}
